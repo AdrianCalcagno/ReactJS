53aeb513001f8487e2157fa63e29f5e1d42d8bb2
AdrianCalcagno <adrianezequielc24@gmail.com>
2022-07-09 22:08:02 -0300

    desafio contador

--- src/App.js
+++ src/App.js
@@ -2,15 +2,20 @@ import './App.css';
 import Navbar from './components/Navbar'
 import CartWidget from './components/CartWidget'
 import ItemListContainer from './components/ItemListContainer'
+import ItemCount from './components/ItemCount'
 
 function App() {
+
+  const onAdd = () => {};
+  
   return (
     <div className="App">
      <Navbar> 
      <CartWidget/>
-      </Navbar>
-     
+    </Navbar>
+  
      <ItemListContainer listContainer="Bienvenido, Luciano"/>
+     <ItemCount stock={5} initial={1}/>
 
     </div>
   );
---
+++ src/components/ItemCount.js
@@ -0,0 +1,68 @@
+import React, { useState } from "react";
+  
+const ItemCount = ({stock, initial, onAdd}) => {
+ const [counter, setCounter] = useState(initial)
+
+ const sumar = () => {
+ if(counter<stock){
+    setCounter(counter + 1)
+  }}
+  
+  const restar = () => {
+    if(counter>initial){
+    setCounter(counter - 1)
+  }}
+  
+  return (
+
+<div className="divContador">
+<h5 style={{
+          fontSize: '200%',
+          marginTop: 20,
+        }}>Contador</h5>
+
+<div style={{
+         fontSize: '200%',
+        }}>{counter}
+</div>
+
+      <div className="buttons">
+        <button style={{
+          fontSize: '200%',
+          backgroundColor: 'white',
+          borderRadius: '8%',
+          color: 'black',
+          marginRight: 15,
+        }}
+          onClick={sumar}>+</button>
+        <button style={{
+          fontSize: '200%',
+          backgroundColor: 'white',
+          borderRadius: '8%',
+          color: 'black',
+        }}
+          onClick={restar}>−</button>
+      </div>
+
+
+      <div className="buttons">
+        
+        <button style={{
+          fontSize: '200%',
+          backgroundColor: 'white',
+          borderRadius: '8%',
+          color: 'black',
+          marginRight: 15,
+        }}
+
+          onClick={onAdd}>Agregar al Carrito</button>
+       
+      </div>
+
+      
+    </div>
+  )
+}
+  
+export default ItemCount;
+
